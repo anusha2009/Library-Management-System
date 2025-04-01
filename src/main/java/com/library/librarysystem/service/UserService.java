fcea8e00d2e8db9ac7f4e9a2b30778691f7d1a81
Anusha A <anushaa@Anushas-MacBook-Air.local>
2025-04-02 00:03:17 +0200

    remove unused comments

--- src/main/java/com/library/librarysystem/service/UserService.java
+++ src/main/java/com/library/librarysystem/service/UserService.java
@@ -7,7 +7,4 @@ import java.util.List;
 public interface UserService {
     User registerUser(User user);
     User findByUsername(String username);
-    //User getUserById(Long id);
-    //List<User> getUsersByRole(String role);
-    //boolean existsByEmail(String email);
 }
